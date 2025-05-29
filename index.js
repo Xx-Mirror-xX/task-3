10bdd1115de10dcc5d750f0c77d2182df7ca8f9a
Xx-Mirror-xX <xxsandovalluisxx@gmail.com>
2025-05-29 04:09:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const app = express();
 require('dotenv').config();
 
-const RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET_KEY || '6Ld6Qk4rAAAAAFIYjMby3XKY70QXNUGNhGFCCwM3';
+const RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET_KEY || '6Le1SE4rAAAAAB07BDfVTCNguDfhgyhJQucCvVYq';
 const ipv6 = '2803:c000:11:850f:4171:f09f:3fb5:73c1';
 const url = `https://ipapi.co/${ipv6}/json/`;
 
