82fb9ec1f4c3039b8d9384e2d04bb32c41267b30
Xx-Mirror-xX <xxsandovalluisxx@gmail.com>
2025-05-30 08:17:11 -0300

    Update ContactsController.js

--- controllers/ContactsController.js
+++ controllers/ContactsController.js
@@ -1,14 +1,12 @@
 const ContactsModel = require('../models/ContactsModel');
-const GeolocationHelper = require('../helpers/geolocation');
-const geolocationHelper = new GeolocationHelper('78fa71af-348c-412f-9a27-15af099c312c');
 const path = require('path');
+
 class ContactsController {
     constructor() {
         this.model = new ContactsModel();
     }
 
-
- async addContact(req, res) {
+    async addContact(req, res) {
         try {
             const { firstName, lastName, email, message } = req.body;
             
@@ -18,31 +16,22 @@ class ContactsController {
                 });
             }
 
+
             const ipAddress = req.ip || 
                              req.headers['x-forwarded-for'] || 
                              req.connection.remoteAddress;
-
-            // Obtener información de geolocalización
-            const location = await geolocationHelper.getLocationByIp(ipAddress);
 
             await this.model.addContact({
                 firstName,
                 lastName,
                 email,
                 message,
-                ipAddress,
-                country: location.country,
-                city: location.city
+                ipAddress
             });
 
             res.status(201).json({ 
                 success: true,
-                message: "Contacto guardado exitosamente",
-                location: {
-                    ip: ipAddress,
-                    country: location.country,
-                    city: location.city
-                }
+                message: "Contacto guardado exitosamente"
             });
         } catch (error) {
             console.error('Error al agregar contacto:', error);
@@ -53,6 +42,24 @@ class ContactsController {
         }
     }
 
+    async getContacts(req, res) {
+        try {
+            if (!req.session.userId) {
+                return res.status(403).json({ 
+                    error: 'Acceso no autorizado' 
+                });
+            }
+            
+            const contacts = await this.model.getAllContacts();
+            res.json(contacts);
+        } catch (error) {
+            console.error('Error al obtener contactos:', error);
+            res.status(500).json({ 
+                error: 'Error al obtener contactos',
+                details: error.message
+            });
+        }
+    }
 
     async index(req, res) {
         if (!req.session.userId) {
