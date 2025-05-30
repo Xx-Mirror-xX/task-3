a12664bb45ce8e7f07fec9b1ffa3ede928077c0e
Xx-Mirror-xX <xxsandovalluisxx@gmail.com>
2025-05-30 08:16:31 -0300

    Update ContactsModel.js

--- models/ContactsModel.js
+++ models/ContactsModel.js
@@ -6,44 +6,46 @@ class ContactsModel {
         this.initTable();
     }
     
-    initTable() {
-        this.db.run(`
-            CREATE TABLE IF NOT EXISTS contacts (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                firstName TEXT NOT NULL,
-                lastName TEXT NOT NULL,
-                email TEXT NOT NULL,
-                message TEXT NOT NULL,
-                ipAddress TEXT NOT NULL,
-                country TEXT,
-                city TEXT,
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
-            )
-        `);
-    }
 
-    async addContact(contactData) {
-        return new Promise((resolve, reject) => {
-            this.db.run(
-                `INSERT INTO contacts 
-                (firstName, lastName, email, message, ipAddress, country, city) 
-                VALUES (?, ?, ?, ?, ?, ?, ?)`,
-                [
-                    contactData.firstName,
-                    contactData.lastName,
-                    contactData.email,
-                    contactData.message,
-                    contactData.ipAddress,
-                    contactData.country,
-                    contactData.city
-                ],
-                function(err) {
-                    if (err) reject(err);
-                    else resolve(this.lastID);
-                }
-            );
-        });
-    }
+initTable() {
+    this.db.run(`
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstName TEXT NOT NULL,
+            lastName TEXT NOT NULL,
+            email TEXT NOT NULL,
+            message TEXT NOT NULL,
+            ipAddress TEXT NOT NULL,
+            country TEXT,
+            city TEXT,
+            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    `);
+}
+
+
+async addContact(contactData) {
+    return new Promise((resolve, reject) => {
+        this.db.run(
+            `INSERT INTO contacts 
+            (firstName, lastName, email, message, ipAddress, country, city) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            [
+                contactData.firstName,
+                contactData.lastName,
+                contactData.email,
+                contactData.message,
+                contactData.ipAddress,
+                contactData.country || 'Desconocido',
+                contactData.city || 'Desconocido'
+            ],
+            function(err) {
+                if (err) reject(err);
+                else resolve(this.lastID);
+            }
+        );
+    });
+}
 
     async getAllContacts() {
         return new Promise((resolve, reject) => {
